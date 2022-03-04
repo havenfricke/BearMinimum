66a94b6c99716e3e4689707b7841bd09286f03db
Gage Lasher <rsgage21@gmail.com>
2022-03-04 15:00:46 -0700

    commit

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -1,5 +1,19 @@
+import { postsService } from "../Services/PostsService.js";
+
+
+
 export class PostsController{
     constructor(){
-        
+        this.getPosts()
+        console.log('loaded the controller');
     }
+
+    async getPosts(){
+        try {
+            await postsService.getPosts()
+        } catch (error) {
+            console.erorr(error);
+        }
+    }
+
 }
--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -1,5 +1,10 @@
-class PostsService{
+import { api } from "./AxiosService.js"
 
+class PostsService{
+    async getPosts(){
+        const res = await api.get('api/posts')
+        console.log(res.data);
+    }
 }
 
 
