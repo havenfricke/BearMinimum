ed55b464a22147f78fb13dc0361b3c72a0a8a371
Haven Fricke <robslaven@live.com>
2022-03-04 14:35:32 -0700

    3.4.22 255pm

--- client/app/main.js
+++ client/app/main.js
@@ -4,6 +4,7 @@ import { ValuesController } from './Controllers/ValuesController.js'
 class App {
   authController = new AuthController();
   valuesController = new ValuesController();
+
 }
 
 // @ts-ignore
